39e1453edcee3a3da4e31ae6a65c716d3c2838f3
Sevdenur <nur.bayram@bil.omu.edu.tr>
2021-02-09 17:12:33 +0300

    homework_8

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -15,11 +15,34 @@ namespace Business.Concrete
             _CarDal = carDal;
         }
 
+        public void Add(Car car)
+        {
+            if (car.DailyPrice > 0 && (car.Description).Length > 2)
+            {
+                _CarDal.Add(car);
+                Console.WriteLine("ekleme işlemi başarılı");
+            }
+            else
+            {
+                Console.WriteLine("Bilgileri kontrol ediniz");
+            }
+        }
+
         public List<Car> GetAll()
         {
             //iş kodları kurallar yetki kontrolleri vs.
             return _CarDal.GetAll();
 
+        }
+
+        public List<Car> GetCarsByBrandId(int id)
+        {
+            return _CarDal.GetAll(c => c.BrandId == id);
+        }
+
+        public List<Car> GetCarsByColorId(int id)
+        {
+            return _CarDal.GetAll(c => c.ColorId == id);
         }
     }
 }
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Business.Concrete;
 using DataAccess.Concrete.InMemory;
+using DataAccess.Concrete.EntityFramework;
 using System;
+using Entities.Concrete;
 
 namespace ConsoleUI
 {
@@ -8,11 +10,27 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            CarManager carManager = new CarManager(new InMemoryCarDal());
+            CarManager carManager = new CarManager(new EfCarDal());
             foreach (var car in carManager.GetAll())
             {
-                Console.WriteLine(car.ModelYear + " " + car.Description + " Daily Price " + car.DailyPrice);
+                Console.WriteLine(car.Description);
             }
+
+            foreach (var car in carManager.GetCarsByBrandId(2))
+            {
+                Console.WriteLine(car.CarId + " " + car.BrandId);
+            }
+
+            foreach (var car in carManager.GetCarsByColorId(3))
+            {
+                Console.WriteLine(car.CarId + " " + car.ColorId);
+            }
+
+            CarManager carManager1 = new CarManager(new EfCarDal());
+
+            carManager1.Add(new Car {BrandId = 1, ColorId = 2,
+                DailyPrice = 650, ModelYear = "2014", Description = "Yeni eklendi"
+            });
         }
     }
 }
