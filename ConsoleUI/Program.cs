3f9a85f674e7858edec7030cd36fa19588ea375d
Sevdenur <nur.bayram@bil.omu.edu.tr>
2021-02-10 10:26:46 +0300

    updated

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -18,18 +18,24 @@ namespace ConsoleUI
 
             foreach (var car in carManager.GetCarsByBrandId(2))
             {
-                Console.WriteLine(car.CarId + " " + car.BrandId);
+                Console.WriteLine(car.CarId + " " + car.Description + " Marka ID:  " + car.BrandId);
             }
 
             foreach (var car in carManager.GetCarsByColorId(3))
             {
-                Console.WriteLine(car.CarId + " " + car.ColorId);
+                Console.WriteLine(car.CarId + car.Description +  " Color ID " + car.ColorId);
             }
 
             CarManager carManager1 = new CarManager(new EfCarDal());
 
-            carManager1.Add(new Car {BrandId = 1, ColorId = 2,
-                DailyPrice = 650, ModelYear = "2014", Description = "Yeni eklendi"
+            carManager1.Add(new Car
+            {
+                CarId = 6,
+                BrandId = 5,
+                ColorId = 2,
+                DailyPrice = 485,
+                ModelYear = "2013",
+                Description = "Astra Dizel Manuel"
             });
         }
     }
--- DataAccess/Concrete/EntityFramework/RentCarDbContext.cs
+++ DataAccess/Concrete/EntityFramework/RentCarDbContext.cs
@@ -10,12 +10,12 @@ namespace DataAccess.Concrete.EntityFramework
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server = (localdb)\v11.0; Database = RentCarDb; Trusted_Connection = true");
+            optionsBuilder.UseSqlServer(@"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = CarRentalDb; Integrated Security = True;");
         }
 
-        public DbSet<Car> Cars { get; set; }
-        public DbSet<Brand> Brands { get; set; }
-        public DbSet<Color> Colors { get; set; }
+        public DbSet<Car> Car { get; set; }
+        public DbSet <Brand> Brand { get; set; }
+        public DbSet<Color> Color { get; set; }
 
     }
 }
